33fc6d0b6510ab3b5653d1f2e1ae3f5cbc2e8f3c
Manoj <pmontu@gmail.com>
2013-06-08 13:15:40 +0530

    fluent & visual - init

---
+++ ContactsFluentMVVM/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace ContactsFluentMVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public ISession session { get; set; }
+        public App()
+        {
+            var sessionFactory = CreateSessionFactory();
+            session = sessionFactory.OpenSession();
+            if (!session.IsConnected)
+            {
+                MessageBox.Show("Unable to connect to database, please make sure PR.db is present in the same directory as the application");
+                Application.Current.Shutdown();
+            }
+        }
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(
+                SQLiteConfiguration.Standard
+                .UsingFile("ContactsDB.db")
+                )
+                .Mappings(m =>
+                            m.FluentMappings.AddFromAssemblyOf<App>())
+                //.ExposeConfiguration(BuildSchema)
+                .BuildSessionFactory();
+        }
+
+        private static void BuildSchema(Configuration config)
+        {
+            // this NHibernate tool takes a configuration (with mapping info in)
+            // and exports a database schema from it
+            new SchemaExport(config)
+              .Create(false, true);
+        }
+    }
+}
---
+++ ContactsFluentMVVM/Domain/People.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace ContactsFluentMVVM.Domain
+{
+    public class People
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Company { get; set; }
+        public virtual string Telephone { get; set; }
+        public virtual string Email { get; set; }
+        public virtual bool? Client { get; set; }
+        public virtual DateTime? LastCall { get; set; }
+    }
+
+    public class PeopleMap : ClassMap<People>
+    {
+        public PeopleMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name).Length(100).Nullable();
+            Map(x => x.Company).Length(100).Nullable();
+            Map(x => x.Telephone).Length(100).Nullable();
+            Map(x => x.Email).Length(100).Nullable();
+            Map(x => x.Client).Nullable();
+            Map(x => x.LastCall).Nullable();
+        }
+    }
+}
